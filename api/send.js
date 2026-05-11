069b6033d0a2e3eed7971ae617b78cac9ad0013e
MD-Bilguun Gansuren <Bilguun.G@mandal.mn>
2026-05-11 18:02:00 +0800

    nice

--- api/send.js
+++ api/send.js
@@ -23,7 +23,16 @@ export default async function handler(req, res) {
 
   let client;
   try {
-    client = new MongoClient(MONGODB_URI);
+    client = new MongoClient(MONGODB_URI, {
+      tls: true,
+      connectTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 10000,
+      serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
     await client.connect();
 
     const database = client.db('insta_login');
